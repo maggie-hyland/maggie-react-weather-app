ac38bd6d91e718d586e45c0409ad91b41b152b66
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2022-10-20 12:26:32 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather Application</div>;
-}
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by Maggie Hyland. Open-sourced on{" "}
+          <a
+            href="https://github.com/maggie-hyland/maggie-react-weather-app"
+            target="-blank"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
+} 
 
 export default App;
