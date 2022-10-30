13a2587725face318ffcf919671324af2782dd13
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2022-10-30 13:38:50 -0400

    updated weather temperature units
    created WeatherTemperature component w/ functions for managing celsius & fahrenheit conversions

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -22,16 +23,12 @@ export default function WeatherInfo(props) {
               />
             </div>
             <div className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: XXX%</li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
