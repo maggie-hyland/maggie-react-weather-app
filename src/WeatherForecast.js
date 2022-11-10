45aed0dde9e7c6d70cf1516367d1186f109b184a
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2022-11-09 20:26:20 -0500

    added useEffect to update forecast when searching for a city

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
+//* need to figure out how to use state management to let the 5-day forecast switch between celsius and fahrenheit *
+
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+//useEffect is needed to change something after the component has already loaded
+//read the following code as: useEffect will setLoaded to false when the props.coordinates change
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+//the purpose of the above code is to allow the 5-day forecast to update when a new city is searched...
+//...this is accomplished by changing the loaded variable to false which will then allow the "else" statement below to call on the api for new data...
+//...which will then update the loaded variable to true and allow the "if" statement below to run
+
   function handleResponse(response) {
     console.log(response.data);
     setForecast(response.data.daily);
